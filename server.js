4ec3882c54f4fac3e548113f7d1a00b7c1a8f490
unknown <Hartie102@gmail.com>
2022-12-09 15:13:08 -0500

    sql connect

--- server.js
+++ server.js
@@ -11,6 +11,22 @@ app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+//sample code to connect to sql
+var connection = new ActiveXObject("ADODB.Connection");
+
+var connectionstring = "Data Source=<server>; Initial Catalog=<catalog>;UserID=<user>;Password=<password>;Provider=SQLOLEDB";
+connection.open(connectionstring);
+var rs = new ActiveXObject("ADODB.Recordset");
+
+rs.Open("SELECT * FROM table",connection);
+rs.MoveFirst
+while(!rs.eof)
+{
+    document.write(rs.fields(1));
+    rs.movenext;
+}
+rs.close;
+connection.close;
 
 app.use('/', indexRouter)
 
